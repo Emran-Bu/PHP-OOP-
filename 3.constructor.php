4c6a819907d0a6fd6d32a98869bb47a3e9caf560
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-10-11 17:23:58 +0600

    constructor

---
+++ 3.constructor.php
@@ -0,0 +1,24 @@
+<?php
+
+    class person {
+        public $name, $age;
+
+        function __construct($name="Emran Hasan", $age=20)
+        {
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        function show () {
+            echo $this->name . " - " . $this->age . "<br>";
+        }
+    }
+
+    $p1 = new person ();
+    $p2 = new person ("Emran", 40);
+    $p3 = new person ("Ehav", 15);
+    // $p1->name = "Emran";
+    // $p1->age = 20;
+    $p1->show();
+    $p2->show();
+    $p3->show();
