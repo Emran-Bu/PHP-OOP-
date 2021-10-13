86638ddc0e0b259a55fc44f6d0c511eab8593a8a
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-10-13 08:39:34 +0600

    magic method

--- 16.magic_method.php
+++ 16.magic_method.php
@@ -12,3 +12,24 @@ class person{
     $test = new person('Emran');
 
 ?>
+
+<!-- magic method list -->
+
+<h1>magic method list</h1>
+
+<ul>
+    <li>__construct()</li>
+    <li>__destruct()</li>
+    <li>__get()</li>
+    <li>__set()</li>
+    <li>__isset()</li>
+    <li>__unset()</li>
+    <li>__autoload()</li>
+    <li>__clone()</li>
+    <li>__sleep()</li>
+    <li>__wakeup()</li>
+    <li>__call()</li>
+    <li>__callStatic()</li>
+    <li>__toString()</li>
+    <li>__invoke()</li>
+</ul>
