70c3b880f669cdfe5c7edb03e3441afc6d7dc62d
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-09-11 00:54:03 +0600

    autoload change

--- 18.autoload/index.php
+++ 18.autoload/index.php
@@ -1,7 +1,8 @@
 <?php
+    // deprecated as of php 7.2.0
 
-    require("class/first.php");
-    require("class/second.php");
+    // require("class/first.php");
+    // require("class/second.php");
 
     function spl__autoload($class){
         require "class/" . $class . ".php";
@@ -11,4 +12,22 @@
     echo "<br>";
     $second = new second();
 
+?>
+
+<?php
+
+    // for php 8.0.0 version
+
+    // require 'class\first.php';
+    // require 'class/second.php';
+
+    spl_autoload_register(function ($class)
+    {
+        echo require 'class/' . $class . '.php';
+    });
+    
+    $f = new first();
+    echo '<br>';
+    $s = new second();
+    
 ?>
